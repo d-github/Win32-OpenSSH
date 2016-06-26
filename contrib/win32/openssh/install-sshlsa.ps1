3e7b7ba137579db5f77b35da859c830df8643a9d
Karl D. Kayat <d-github@bitpri.de>
2016-06-27 01:32:52 +0200

    Fix ssh-lsa install issue 142

--- contrib/win32/openssh/install-sshlsa.ps1
+++ contrib/win32/openssh/install-sshlsa.ps1
@@ -1,7 +1,7 @@
 ﻿copy .\ssh-lsa.dll $env:windir\system32
 $subkey = 'SYSTEM\CurrentControlSet\Control\Lsa'
 $value  = 'Authentication Packages'
-$reg = [Microsoft.Win32.RegistryKey]::OpenBaseKey('LocalMachine', 0)
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', 'localhost')
 $key = $reg.OpenSubKey($subkey, $true)
 $arr = $key.GetValue($value)
 if ($arr -notcontains 'ssh-lsa') {
